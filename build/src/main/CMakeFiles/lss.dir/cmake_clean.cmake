29659a89f2b83313f8b3e225b8f86827c7dee65a
Czxxx <1690159101@qq.com>
2024-06-04 18:43:49 +0800

    update build

---
+++ build/src/main/CMakeFiles/lss.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lss.dir/main.cpp.o"
+  "CMakeFiles/lss.dir/main.cpp.o.d"
+  "lss"
+  "lss.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lss.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
