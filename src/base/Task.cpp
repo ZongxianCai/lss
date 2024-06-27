5bfe7f50cec86c056abe3809697ff6e0933a035b
ZongxianCai <1690159101@qq.com>
2024-06-27 23:28:11 +0800

    Add Task class implementation and header

---
+++ src/base/Task.cpp
@@ -0,0 +1,34 @@
+#include "Task.h"
+#include "TTime.h"
+
+using namespace lss::base;
+
+// 回调构造函数的实现
+Task::Task (const TaskCallback &cb, int64_t interval)
+: interval_(interval), when_(TTime::NowMS() + interval), cb_(cb)
+{
+
+}
+
+// 右值回调构造函数的实现
+Task::Task (const TaskCallback &&cb, int64_t interval)
+: interval_(interval), when_(TTime::NowMS() + interval), cb_(std::move(cb))
+{
+
+}
+
+// 执行任务方法的实现
+void Task::Run()
+{
+    if (cb_)
+    {
+        cb_(shared_from_this());
+    }
+}
+
+// 重启任务方法的实现
+void Task::Restart()
+{
+    // 更新时间点
+    when_ = interval_ + TTime::NowMS();
+}
---
+++ src/base/Task.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <cstdint>
+#include <functional>
+#include <memory>
+
+namespace lss
+{
+    namespace base
+    {
+        // 前置声明
+        class Task;
+        using TaskPtr = std::shared_ptr<Task>;
+        using TaskCallback = std::function<void (const TaskPtr &)>;
+
+        // 将类本身作为一个智能指针
+        class Task : public std::enable_shared_from_this<Task>
+        {
+        public:
+            // 回调构造函数
+            Task (const TaskCallback &cb, int64_t intervel);
+
+            // 右值回调构造函数
+            Task (const TaskCallback &&cb, int64_t intervel);
+
+            // 执行任务的方法
+            void Run();
+
+            // 重启任务的方法
+            void Restart();
+        
+        private:
+            // 时间间隔
+            int64_t interval_{0};
+            // 执行任务的时间点（当前时间 + 时间间隔得到）
+            int64_t when_{0};
+            // 回调函数
+            TaskCallback cb_;
+        };
+    }
+}
