7f808fa98fba6d7a07334d58dbc7631280081ee9
ZongxianCai <1690159101@qq.com>
2024-06-28 00:58:40 +0800

    fix bug

--- src/base/TaskManager.h
+++ src/base/TaskManager.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <unordered_set>
+#include <mutex>
 #include "Task.h"
 #include "Singleton.h"
 
@@ -6,14 +9,33 @@ namespace lss
 {
     namespace base
     {
+        // 定时任务管理器类
         class TaskManager : public NonCopyable
         {
         public:
-            // 使用默认构造函数
+            // 使用默认构造函数，不进行任何操作
             TaskManager() = default;
 
-            // 使用默认析构函数
+            // 使用默认析构函数，不进行任何操作
             ~TaskManager() = default;
+
+            // 存放所有的定时任务
+            void OnWork();
+
+            // 添加定时任务
+            bool Add(TaskPtr &task);
+
+            // 删除定时任务
+            bool Del(TaskPtr & task);
+
+        private:
+            // 存储和管理任务，存储Task类型的智能指针
+            std::unordered_set<TaskPtr> tasks_;
+
+            // 互斥锁，用于同步访问任务集合，保证多线程环境下的线程安全
+            std::mutex lock_;
         };
     }
+    
+    #define taskManager lss::base::Singleton<lss::base::TaskManager>::Instance()
 }
