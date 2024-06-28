0b6bb77b6f3991c1ed99ea7e2c0f304e365417c7
ZongxianCai <1690159101@qq.com>
2024-06-28 17:33:44 +0800

    update comments

--- src/base/Task.h
+++ src/base/Task.h
@@ -10,20 +10,22 @@ namespace lss
     {
         // 前置声明
         class Task;
+        // 定义 TaskPtr 类型别名，是 Task 类的 std::shared_ptr，用于自动管理 Task 对象的生命周期
         using TaskPtr = std::shared_ptr<Task>;
+        // 定义 TaskCallback 类型别名，是 std::function 的特化版本，表示接受 const TaskPtr & 作为参数并返回 void 的函数或函数对象。
         using TaskCallback = std::function<void (const TaskPtr &)>;
 
-        // 将类本身作为一个智能指针
+        // 定义 Task 类，继承自 std::enable_shared_from_this，允许 Task 对象通过 shared_from_this 方法获取自身的 shared_ptr
         class Task : public std::enable_shared_from_this<Task>
         {
         public:
-            // 回调构造函数
-            Task(const TaskCallback &cb, int64_t intervel);
+            // 构造函数，接受 TaskCallback 类型的引用和 int64_t 类型的间隔，用于初始化任务的回调和执行间隔
+            Task(const TaskCallback &cb, int64_t interval);
 
-            // 右值回调构造函数
-            Task(const TaskCallback &&cb, int64_t intervel);
+            // 接受 TaskCallback 类型的右值引用，允许使用临时的或右值引用的回调函数
+            Task(const TaskCallback &&cb, int64_t interval);
 
-            // 执行任务的方法
+            // 执行任务的回调函数
             void Run();
 
             // 重启任务的方法
--- src/base/TaskManager.cpp
+++ src/base/TaskManager.cpp
@@ -6,23 +6,30 @@ using namespace lss::base;
 // 存放所有的定时任务
 void TaskManager::OnWork()
 {
-    // 获取锁，RAII
+    // RAII（资源获取即初始化），lk对象在构造时自动获取 lock_ 互斥锁，并在析构时释放锁
     std::lock_guard<std::mutex> lk(lock_);
     // 获取当前时间，与任务时间点进行对比
     int64_t now = TTime::NowMS();
 
+    // 遍历tasks_中的所有任务
     for (auto iter = tasks_.begin(); iter != tasks_.end();)
     {
+        // 检查当前迭代的任务是否到达执行时间
         if ((*iter)->When() < now)
         {
+            // 执行任务
             (*iter)->Run();
 
+            // 再次检查任务的执行时间是否小于当前时间，以确保任务不会重复执行
             if ((*iter)->When() < now)
             {
+                // 如果任务的执行时间小于当前时间，从 tasks_ 中删除这个任务，并继续下一个迭代
                 iter = tasks_.erase(iter);
                 continue;
             }
         }
+
+        // 如果任务还没有到执行时间，移动迭代器到下一个任务
         iter++;
     }
 }
@@ -30,29 +37,32 @@ void TaskManager::OnWork()
 // 添加定时任务
 bool TaskManager::Add(TaskPtr &task)
 {
-    // 获取锁，RAII
+    // 获取互斥锁，RAII
     std::lock_guard<std::mutex> lk(lock_);
+    // 在tasks_中查找任务，如果任务已经存在，返回迭代器
     auto iter = tasks_.find(task);
 
-    // 找到了，返回false
+    // 任务存在，返回false
     if (iter != tasks_.end())
     {
         return false;
     }
 
-    // 未找到，进行插入
+    // 如果任务不存在，使用 emplace 方法将任务添加到 tasks_ 中
     tasks_.emplace(task);
 
+    // 任务添加成功
     return true; 
 }
 
 // 删除定时任务
 bool TaskManager::Del(TaskPtr & task)
 {
-    // 获取锁，RAII
+    // 获取互斥锁，RAII
     std::lock_guard<std::mutex> lk(lock_);
-    // 删除
+    // 删除指定任务
     tasks_.erase(task);
 
+    // 任务删除成功
     return true;
 }
