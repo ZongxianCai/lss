ff14fcebc3b9c0f277de77c543dde141e7bbe3bb
ZongxianCai <1690159101@qq.com>
2024-07-27 12:25:30 +0800

    add EventLoopThread

---
+++ src/network/net/EventLoopThread.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "base/NonCopyable.h"
+#include "EventLoop.h"
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+
+namespace lss
+{
+    namespace network
+    {
+        class EventLoopThread
+        {
+        public:
+            EventLoopThread();
+
+            ~EventLoopThread();
+
+            void Run();
+
+            EventLoop *loop() const;
+
+        private:
+            void StartEventLoop();
+
+            EventLoop *loop_{nullptr};
+
+            std::thread thread_;
+
+            std::mutex lock_;
+
+            std::condition_variable condition_;
+
+            bool running_{false};
+
+            std::once_flag once_;
+
+            std::promise<int> promise_loop_;
+        };
+    }
+}
