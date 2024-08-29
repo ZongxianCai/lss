b2a5cb2030946837af2d08b6f26ffd8db7c60fc4
czx <1690159101@qq.com>
2024-08-29 19:12:39 +0800

    test timeout function

--- src/network/net/tests/TcpConnectionTest.cpp
+++ src/network/net/tests/TcpConnectionTest.cpp
@@ -1,4 +1,3 @@
-/*
 #include <iostream>
 #include "network/net/Acceptor.h"
 #include "network/net/EventLoop.h"
@@ -78,53 +77,5 @@ int main(int argc, const char *argv[])
             std::this_thread::sleep_for(std::chrono::seconds(1));
         }
     }
-    return 0;
-}
-*/
-
-
-
-#include "network/net/Acceptor.h"
-#include "network/net/EventLoop.h"
-#include "network/net/EventLoopThread.h"
-#include "network/net/TcpConnection.h"
-#include <iostream>
-
-using namespace lss::network;
-EventLoopThread eventloop_thread;
-std::thread th;
-
-const char *http_response="HTTP/1.0 200 OK\r\nServer: lss\r\nContent-Type: text/html\r\nContent-Length: 0\r\n\r\n";
-int main(int argc,const char ** agrv)
-{
-    eventloop_thread.Run();
-    EventLoop *loop = eventloop_thread.Loop();
-
-    if(loop)
-    {   std::vector<TcpConnectionPtr> list;
-        InetAddress server("172.22.88.236:34444");
-        std::shared_ptr<Acceptor> acceptor = std::make_shared<Acceptor>(loop,server);
-        acceptor->SetAcceptCallback([&loop,&server,&list](int fd,const InetAddress &addr){
-            std::cout << "host:" << addr.ToIpPort() << std::endl;
-            TcpConnectionPtr connection = std::make_shared<TcpConnection>(loop,fd,server,addr);
-            connection->SetRecvMsgCallback([](const TcpConnectionPtr&con,MsgBuffer &buf){
-                std::cout << "recv msg:" << buf.Peek() << std::endl;
-                buf.RetrieveAll();
-                con->Send(http_response,strlen(http_response));
-            });
-            connection->SetWriteCompleteCallback([&loop](const TcpConnectionPtr&con){
-                std::cout << "write complete host:" << con->PeerAddr().ToIpPort() << std::endl;
-                loop->DelEvent(con);
-                con->ForceClose();
-            });
-            list.push_back(connection);
-            loop->AddEvent(connection);
-        });
-        acceptor->Start();
-        while(1)
-        {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-    }    
     return 0;
 }
